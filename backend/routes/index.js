ced7ed63e4377cf29d5bd54f9471605ffee75e1e
BarbaraFerlinc <barbara.ferlinc@student.um.si>
2023-05-14 20:58:34 +0200

    metode in knex

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: "root",
+        database: 'recloth_db'
+    }
+});
+
 // metode
+// filtriranje artiklov glede na kategorijo
 
 module.exports = router;
--- backend/routes/uporabnik.js
+++ backend/routes/uporabnik.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: "root",
+        database: 'recloth_db'
+    }
+});
+
 // metode
+// shrani/uredi/zbrisi uporabnika
 
 module.exports = router;
