960e1d71e52e64f7885214029a98c512472e0893
BarbaraFerlinc <barbara.ferlinc@student.um.si>
2023-05-15 13:03:35 +0200

    popravljeno za bazo

--- backend/routes/profil.js
+++ backend/routes/profil.js
@@ -2,17 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var knex = require('knex')({
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
         host: '127.0.0.1',
         user: 'root',
-        password: "root",
+        password: 'root',
         database: 'recloth_db'
     }
 });
 
 // metode
 // get enega uporabnika
-// filtriranje artiklov glede na uporabnika
+// filtriranje artiklov glede na uporabnika (get)
 
 module.exports = router;
