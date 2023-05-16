a5fdfc3b20628a2c8e9cdc5002f2d23b2995a10e
LukaPrepadnik <luka.prepadnik@student.um.si>
2023-05-16 11:47:45 +0200

    get za kategorije

--- backend/routes/kategorija.js
+++ backend/routes/kategorija.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../knexConfig')
-// metode
-// get vse kategorije
-// filtriranje artiklov glede na kategorijo (get)
+
+
+router.get('/vsi', async (req, res) => {
+    try {
+        const kategorije = await knex('kategorija').select('*');
+        res.status(200).json(kategorije);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Napaka pri pridobivanju kategorij iz baze', details: error.message });
+    }
+});
+
+
+
+
+
 
 module.exports = router;
